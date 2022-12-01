0441214f60a4cab533c52ea798c9bd15399e1142
Hadymohammed <Hadymohamed2002@gmail.com>
2022-12-01 21:32:45 +0200

    ready to deploy

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,12 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=abdelhady
+export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=admin
 export POSTGRES_HOST=127.0.0.1
 ##export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
 export PORT=5432
 export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::abdelhady-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -11,11 +11,11 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD as string,
   database: process.env.POSTGRES_DB,
   port: Number(process.env.POSTGRES_PORT),
-  host:process.env.POSTGRES_HOST,/*
+  host:process.env.POSTGRES_HOST,
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,*/
+  url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
