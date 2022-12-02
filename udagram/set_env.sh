629d11458bc6e62599af1472fa1f3ec7342fe119
Hadymohammed <Hadymohamed2002@gmail.com>
2022-12-03 00:26:32 +0200

    application is live!

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=admin
-export POSTGRES_HOST=127.0.0.1
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.c4kyfxhlmjqz.us-east-1.rds.amazonaws.com
 ##export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
 export PORT=5432
-export POSTGRES_DB=udagram
+export POSTGRES_DB=udagramDB
 export AWS_BUCKET=arn:aws:s3:::abdelhady-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://localhost:4200
