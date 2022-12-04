c11f758e5ba9050c87d704f3756b9bd703d38ff8
Hadymohammed <Hadymohamed2002@gmail.com>
2022-12-04 09:18:44 +0200

    update setenv script & config.yml workflow

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.c4kyfxhlmjqz.us-east-1.rds.amazonaws.com
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
 ##export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
-export PORT=5432
-export POSTGRES_DB=udagramDB
-export AWS_BUCKET=arn:aws:s3:::abdelhady-udagram
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:4200
+eb setenv POSTGRES_PORT=$POSTGRES_PORT
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv AWS_S3_BUCKET=$AWS_S3_BUCKET
+eb setenv AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv JWT_SECRET=$JWT_SECRET
